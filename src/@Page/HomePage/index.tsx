c90dede9f2b905e119cee2dace99deb9bea6cc90
Truong Pham <truongpham2412.nd@gmail.com>
2024-02-20 09:22:39 +0700

    init

---
+++ src/@Page/HomePage/index.tsx
@@ -0,0 +1,6 @@
+import type { NextPage } from "next";
+const HomePage: NextPage = () => {
+  return <div>1</div>;
+};
+
+export default HomePage;
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import HomePage from "@/@Page/HomePage";
+
+export const metadata = {
+  title: "Educenter",
+  description: ":)) ",
+};
+export default HomePage;
